960d45e10ac37abd3afa9cfa488d22d027c34cf8
Huỳnh Ngọc Phương Nghi <pniamie@MacBook-Air-cua-Huynh-2.local>
2026-01-19 23:14:32 +0700

    Upload my-app code

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Contact } from './contact/contact';
+import { Ptb1 } from './ptb1/ptb1';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Contact,
+    Ptb1
   ],
   imports: [
     BrowserModule,
